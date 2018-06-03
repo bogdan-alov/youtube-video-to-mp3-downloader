71a5e93ff6e8fa215a96ab24670a3c5df3422657
Bogdan Alov <alovbg21@abv.bg>
2018-06-03 20:14:34 +0300

    adding some error handling

--- src/youtube_downloader.py
+++ src/youtube_downloader.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 import youtube_dl
 import argparse
+import os,sys
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Downloads a selected youtube video or playlist')
 parser.add_argument('--url', nargs='?', help='Video url');
@@ -17,5 +19,10 @@ ydl_opts = {
 
 while True:
 	video_url = raw_input("Paste link here: ")
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		ydl.download([video_url])
+	try:
+		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+			ydl.download([video_url])
+	except Exception as e:
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' Type:{} Filename:{} Line:{} ERROR: {}'.format(exc_type, fname, exc_tb.tb_lineno,e))
